9165f3ca4135da532e91e30650fce43726c32bc4
Yang Zhang <30611935+YangZhangHaveFun@users.noreply.github.com>
2019-06-02 02:48:01 +1000

    Update TestController.java

--- src/main/java/com/yzzhan/concurrencyPractice/TestController.java
+++ src/main/java/com/yzzhan/concurrencyPractice/TestController.java
@@ -1,12 +1,15 @@
 package com.yzzhan.concurrencyPractice;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 public class TestController {
+
+
 
     @RequestMapping("/test")
     public String test(){
